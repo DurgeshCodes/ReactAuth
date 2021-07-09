213ec2e1860ff84b25c13978d72e5dbc582a8724
Durgesh Nandan Birmiwal <Durgeshagrawal38@gmail.com>
2021-07-09 19:12:24 +0530

    signup working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
-
+import SignIn from "./components/Pages/SignIn";
+import SignUp from "./components/Pages/SignUp";
+import { AuthProvider } from "./contexts/authContext";
+console.log(
+  process.env.REACT_APP_FIREBASE_API_KEY,
+  process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  process.env.REACT_APP_FIREBASE_MESSEGING_SENDER_ID,
+  process.env.REACT_APP_FIREBASE_APP_ID
+);
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AuthProvider>
+      <div className="App">
+        <SignUp />
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Pages/SignUp.js
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Alert from "@material-ui/lab/Alert";
+
+import { useAuth } from "../../contexts/authContext";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { signup, currentUser } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password, confirmPassword } = e.currentTarget.elements;
+    console.log(email.value, password.value, confirmPassword.value);
+    if (password.value !== confirmPassword.value) {
+      return setError("Passwords do not match ");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(email.value, password.value);
+    } catch {
+      setError("Failed to Create an Account");
+    }
+    setLoading(false);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        {error && <Alert severity="error">{error}</Alert>}
+        <h1>{currentUser?.email}</h1>
+        <form onSubmit={handleSubmit} className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                label="Email Address"
+                autoComplete="email"
+                name="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                label="Password"
+                type="password"
+                name="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                label="Confirm Password"
+                type="password"
+                name="confirmPassword"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={loading}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
